899474411d3a2142b1ed4bfd465d32c2597c86b3
Gabriel <gabsanchez438@gmail.com>
2018-09-04 23:57:22 -0600

    nav links fixeados

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-/*var navBar = document.getElementById("soccer-nav");
-var link1 = document.getElementById("link1");
-var link2 = document.getElementById("link2");
-var link3 = document.getElementById("link3");
-var jumbo = document.getElementById("principal");
-var tema = 0;*/
+cambiarTema(0);
 function cambiarTema(option){
     switch(option){
         case 0:
@@ -12,6 +7,19 @@ function cambiarTema(option){
             document.getElementById("link2").classList.add("nav-link--soccer-site--default");
             document.getElementById("link3").classList.add("nav-link--soccer-site--default");
             document.getElementById("principal").classList.add("jumbotron--default");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--one");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--one");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--one");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--one");            
+            document.getElementById("principal").classList.remove("jumbotron--one");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--two");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--two");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--two");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--two");            
+            document.getElementById("principal").classList.remove("jumbotron--two");
+            
             break;
         case 1:
             document.getElementById("soccer-nav").classList.add("soccer-nav--one");
@@ -19,6 +27,18 @@ function cambiarTema(option){
             document.getElementById("link2").classList.add("nav-link--soccer-site--one");
             document.getElementById("link3").classList.add("nav-link--soccer-site--one");
             document.getElementById("principal").classList.add("jumbotron--one");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--default");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--default");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--default");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--default");            
+            document.getElementById("principal").classList.remove("jumbotron--default");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--two");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--two");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--two");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--two");            
+            document.getElementById("principal").classList.remove("jumbotron--two");
             break;
         case 2:
             document.getElementById("soccer-nav").classList.add("soccer-nav--two");
@@ -26,6 +46,18 @@ function cambiarTema(option){
             document.getElementById("link2").classList.add("nav-link--soccer-site--two");
             document.getElementById("link3").classList.add("nav-link--soccer-site--two");
             document.getElementById("principal").classList.add("jumbotron--two");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--one");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--one");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--one");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--one");            
+            document.getElementById("principal").classList.remove("jumbotron--one");
+
+            document.getElementById("soccer-nav").classList.remove("soccer-nav--default");
+            document.getElementById("link1").classList.remove("nav-link--soccer-site--default");
+            document.getElementById("link2").classList.remove("nav-link--soccer-site--default");
+            document.getElementById("link3").classList.remove("nav-link--soccer-site--default");            
+            document.getElementById("principal").classList.remove("jumbotron--default");
             break;
     }
 }
---
+++ js/scopeVars.js
@@ -0,0 +1,5 @@
+var navBar = document.getElementById("soccer-nav");
+var link1 = document.getElementById("link1");
+var link2 = document.getElementById("link2");
+var link3 = document.getElementById("link3");
+var jumbo = document.getElementById("principal");
